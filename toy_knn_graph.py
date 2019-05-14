ab0e57da6063a1dfaedd67b02ab51a567ec07d0d
Not Shreyan Jain <nilaisarda@gmail.com>
2019-05-14 04:41:15 +0000

    toy knn graph gen

--- toy_knn_graph.py
+++ toy_knn_graph.py
@@ -3,7 +3,6 @@ import sklearn
 import pickle
 
 import numpy as np
-import matplotlib.pyplot as plt
 import networkx as nx
 
 from tqdm import tqdm
@@ -11,17 +10,19 @@ from energyflow.emd import emd, emds
 from energyflow.datasets import qg_jets
 
 N = 1000
-M = 8
+M = 200
 c = 4
 d = 2
 k = 10
 S = 20 # need S-nn graph for soft label empirical distribution over S nearest neighbors, S >> k
 
+a = 2.5
+
 def dist(x, y):
 	return np.linalg.norm(x-y)
 
 cov1 = np.array([[1, 0], [0, 1]])
-centres = np.array([[5, 5], [-5, 5], [5, -5], [-5, -5]])
+centres = np.array([[a, a], [-a, a], [a, -a], [-a, -a]])
 means = np.random.uniform(-0.5, 0.5, size=(c*N, d))
 
 data = []
@@ -36,18 +37,25 @@ xs = data.reshape(-1, 2)
 pickle.dump(data, open("pickles/bigger_toy_points.pickle", "wb"))
 knn = []
 all_distances = np.zeros((c*N, c*N))
+from multiprocessing import Pool
+pool = Pool(processes=16)
 for i in tqdm(range(c*N)):
     distances = []
-    for j in range(c*N):
-        mu_i, mu_j = data[i], data[j]
-        C = np.zeros((M, M))
+    mu_i = data[i]
+    
+    def f(mu_j):
+    	C = np.zeros((M, M))
         for x in range(M):
             for y in range(M):
                 C[x, y] = dist(mu_i[x], mu_j[y])
         d = ot.emd2([], [], C)
-        distances.append((j,d))
-        all_distances[i, j] = d
-        distances.sort(key=lambda x: x[1])
+        return (j,d)
+   	
+    distances = pool.map(f, data)
+    for j in range(len(distances)):
+	    all_distances[i, distances[j][0]] = distances[j][1]
+
+    distances.sort(key=lambda x: x[1])
     for v in distances[:k]:
         knn.append((i, v[0], {'weight': v[1]}))
 
